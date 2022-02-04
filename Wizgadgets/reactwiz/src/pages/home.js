f9dfa9d05386d1465df54cbb40b84350e7df97bd
Abinesh <Abinesh@LAPTOP-T5VCQHCA>
2022-02-04 15:54:27 +0530

    Add project files.

---
+++ Wizgadgets/reactwiz/src/pages/home.js
@@ -0,0 +1,160 @@
+import React from 'react';
+
+class Home extends React.Component{
+	render(){
+		return(
+
+			<div>
+		<title>WIZ GADGETS</title>
+	<div class="total">
+		<div class="logo">
+			<img src="assets/images4/logoo.png" />
+		</div>
+		<div class ="navigation">
+			    <nav>
+				     <ul>
+				        <a href="#"><li><b><u>HOME</u></b></li></a>
+			            <a href="#"><li ><b>PRODUCTS</b></li></a>
+			            <a href="#"><li ><b>ABOUT</b></li></a>
+			            <a href="#"><li ><b>CONTACT</b></li></a>
+			           	<a href="#"><li ><b>ACCOUNT</b></li></a>
+			           	<a href="#"><li><ion-icon name="search-outline"></ion-icon></li></a>
+			           	<a href="#"><li><ion-icon name="cart-outline"></ion-icon></li></a>	
+  
+                     </ul>
+                </nav>
+
+			
+		</div>
+	</div>
+
+
+	<div class="banner">
+		<h1>SHOP WITH US</h1>
+		<p> Magic can do wonders, Unfortunately it is not real but tecnology is real.<br/>
+		 Let us help you to bring magic in your life...</p><br/>
+		 <div class="button1">
+		 	<button> <b><b>Shop Now > </b></b></button>
+		 	
+		 </div>
+		 <div class="button2">
+		 	<button><b>Join premium</b></button>
+		 </div>		
+		 	
+		 
+	</div><br/>
+	<div class="box">
+		<div class="heading1">
+			<h2><b>Featured Products</b></h2>
+		</div><br/><br/>
+				
+					<div class="inbox">
+						<div class="item1">
+							<p>Smart Watches</p>
+						</div>
+						<div class="item2">
+							<p>mobiles</p>
+						</div>
+						<div class="item3">
+							<p>	Laptops</p>
+						</div>
+						
+					</div>
+	</div><br/><br/>
+	<div class="box1">
+					<div class="heading2">
+						<h2><b>Latest Products</b></h2>
+					</div><br/><br/>	
+				
+					<div class="inbox1">
+						<div class="product1">
+							<img src="assets/images4/item1.jpg" />
+						</div>
+						<div class="product2">
+							<img src="assets/images4/item3.jpg" />
+						</div>
+						<div class="product3">
+							<img src="assets/images4/item2.jpg" />
+						</div>
+						<div class="product4">
+							<img src="assets/images4/item4.jpg" />
+						</div>
+						
+					</div><br/><br/>
+					<div class="inbox2">
+						<div class="product5">
+							<img src="assets/images4/item5.jpg" />
+						</div>
+						<div class="product6">
+							<img src="assets/images4/item7.jpg" />
+						</div>
+						<div class="product7">
+							<img src="assets/images4/item6.jpg" />
+						</div>
+						<div class="product8">
+							<img src="assets/images4/item8.jpg" />
+						</div>
+						
+					</div>
+
+	</div><br/><br/>
+	<div class="banner2">
+		<p></p>
+	</div><br/><br/><br/><br/>
+
+	<div class="icons">
+
+		<div class="oneplus">
+			<button></button>	
+		</div>
+		<div class="apple">
+			<button></button>	
+		</div>
+		<div class="sony">
+			<button></button>	
+		</div>
+		<div class="samsung">
+			<button></button>	
+		</div>
+		<div class="vivo">
+			<button></button>	
+		</div>
+
+	</div><br/><br/><br/><br/><br/><br/><br/><br/>
+	<div class="footer">
+		<div class="footerimg">
+			<img src="assets/images4/logoo.png" />
+		</div>
+		<div class="content">
+			<p> Magic can do wonders, Unfortunately it is not real but tecnology is real.<br/>
+		 Let us help you to bring magic in your life...</p>
+		</div>
+		<div class="socicon">
+			<h3>follow us :</h3>
+			<a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+			<a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+			<a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a>
+			<a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+		</div>
+		<div class="playstore">
+			<h3>Download App :</h3>
+			<a href="#"><ion-icon name="logo-google-playstore"></ion-icon></a>
+		</div>
+		<div class="tollfree">
+			<h2>For more details</h2>
+			<p>Toll-Free no : +1-866-832-3090 </p>
+		</div>
+		<hr/>
+		<div class="copyright">
+			<p>Copyright 2021 - Our Terms & Conditions Generator can help you generate a customized Terms & Conditions agreement in around three minutes, for free</p>
+		</div>
+		
+	</div>
+
+		    </div>
+
+		);
+	}
+}
+
+export default Home;  
